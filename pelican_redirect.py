f00004c2f418232bd64f11ab549dbfd359ddee7a
Paul Winkler <slinkp@gmail.com>
2016-01-17 02:11:29 -0500

    Fix pelican-redirect to use correct output paths - overwrite previously saved Articles or Pages.

--- pelican_redirect.py
+++ pelican_redirect.py
@@ -1,7 +1,7 @@
 from pelican import signals
 from pelican.contents import Content
 from pelican.readers import BaseReader
-from pelican.generators import Generator, _FileLoader
+from pelican.generators import Generator
 from pelican.utils import pelican_open
 import email.parser
 import logging
@@ -43,6 +43,17 @@ class RedirectReader(BaseReader):
             'location': location,
             'delay': delay,
         }
+
+        # Slug is important because it Pelican's slugification affects
+        # the final URL, and we care about exact URLs here.
+        # So for redirect files, we assume that you named them carefully.
+        # And allow overriding by explicit slug.
+        slug = message.get('slug')
+        if not slug:
+            slug = os.path.splitext(os.path.basename(source_path))[0]
+        if slug:
+            metadata['slug'] = slug
+
         content = message.get_payload().strip()
         return content, metadata
 
@@ -59,12 +70,26 @@ class RedirectGenerator(Generator):
         # is documented as taking a list of extensions but it actually
         # only takes a string. Report upstream.
         # Hacking around it.
+        # XXX ... or no; 'foo'.endswith(('a', 'x', 'o')) -> True
         for extension in RedirectReader.file_extensions:
             redirect_input_files.update(
                 self.get_files(paths=input_paths, extensions=extension)
             )
 
         redirects = []
+
+        # By the time this runs, the default generators have already
+        # run and created Page instances for all .302 files,
+        # and those have correct url path calculations.
+        # We iterate over all of those, and steal the correct paths.
+        # Dirty hack :)
+        source_path_to_final_path = {}
+        for type_key in ('pages', 'articles'):
+            content_records = self.context.get(type_key)
+            for cr in content_records or []:
+                # Get all redirects seen so far.
+                if cr.reader == 'redirect':
+                    source_path_to_final_path[cr.source_path] = cr.save_as
 
         for f in redirect_input_files:
             try:
@@ -82,26 +107,34 @@ class RedirectGenerator(Generator):
                              exc_info=self.settings.get('DEBUG', False))
                 self._add_failed_source_path(f)
                 continue
+
+            if redirect.source_path in source_path_to_final_path:
+                redirect.override_save_as = source_path_to_final_path[
+                    redirect.source_path]
+
             self.add_source_path(redirect)
             redirects.append(redirect)
 
-        print "Got a buncha redirects: %s" % redirects
+        # print "Got a buncha redirects: %s" % redirects
         self.context['redirects'] = redirects
 
     def generate_output(self, writer):
         for source in self.context['redirects']:
-            dest = source.save_as  # TODO?
+            dest = source.save_as
             template = self.get_template('redirect')
-            rurls = self.settings['RELATIVE_URLS']
 
             # We need to override the file because I haven't found a way
-            # to prevent the Page / Article generators from writing it
-            # before we get here.
+            # to prevent the Page / Article generators from writing the
+            # .302 article as a normal (empty) Page before we get here.
             # You can do that by directory (via ARTICLE_EXCLUDES) but not
             # by file extension, apparently.
             # So this hack prevents an error if the file is already
             # marked as overridden.
-
+            #
+            # ... Unfortunately, dest doesn't match the path of Pages
+            # nested in subdirs. So this goes in the wrong place
+            # and the Page is still there in the wrong place.
+            # Where/ how do they handle subdirs?
             try:
                 writer._overridden_files.remove(
                     os.path.join(writer.output_path, dest))
@@ -109,9 +142,22 @@ class RedirectGenerator(Generator):
                 pass
 
             writer.write_file(
-                dest, template, self.context, rurls,
+                dest, template, self.context,
                 page=source,
+                relative_urls=self.settings['RELATIVE_URLS'],
                 override_output=True)
+
+    def generate_feeds(self):
+        """
+        Redirects don't go in any feeds.
+        """
+        return
+
+    def generate_period_archives(self):
+        """
+        Redirects don't go in any archives.  (I think.)
+        """
+        return
 
 
 def add_reader(readers):
